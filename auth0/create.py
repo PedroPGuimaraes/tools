a4fcb317e0a948c21c57450ab449255462e80471
Pedro P. Guimarães <151934241+PedroPGuimaraes@users.noreply.github.com>
2025-03-13 16:11:07 -0300

    fix: updated shiny dash load

--- auth0/create.py
+++ auth0/create.py
@@ -33,7 +33,7 @@ def create_user(email, dashes, client_id, client_secret):
 
     password = generate_secure_password()
 
-    shinyproxy_roles = dashes.split(',')
+    shinyproxy_roles = json.loads(dashes)
 
     payload = json.dumps({
         "email": email,
